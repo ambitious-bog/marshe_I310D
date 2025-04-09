634b02eeee385bc20e9d4cfcc9e561645575c8cd
ambitious-bog <mvo1@utexas.edu>
2025-04-09 09:57:31 -0500

    Wrote function for formatting circle area string

--- area_of_a_circle.py
+++ area_of_a_circle.py
@@ -3,10 +3,13 @@ def compute_area_of_circle(radius):
 	area = pi * radius * radius
 	return area
 
+def circ_area_str(radius, area):
+	return f"The area of circle with radius {radius} is: {area}"
+
 radius1 = 30
 area1 = compute_area_of_circle(radius1)
-print(f"The area of circle with radius {radius1} is: {area1}")
+print(circ_area_str(radius1, area1))
 
 radius2 = 40
 area2 = compute_area_of_circle(radius2)
-print(f"The area of circle with radius {radius2} is: {area2}")
+print(circ_area_str(radius2, area2))
